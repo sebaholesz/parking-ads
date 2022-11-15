421c7db462f62da147d96fdd5c7fad1dfd5130a9
Zack <zahariahoratau@gmail.com>
2022-11-16 00:26:48 +0100

    Added ParkingSpot domain class.

---
+++ ParkingService/Domain/ParkingSpot.cs
@@ -0,0 +1,15 @@
+﻿namespace ParkingService.Domain;
+
+public class ParkingSpot
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string StreetName { get; set; }
+    public string StreetNumber { get; set; }
+    public string Details { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public string ZipCode { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime LastUpdated { get; set; }
+}
