2b6cdd2cb20ac4921744c6396606dc5e5def6203
Zack <zahariahoratau@gmail.com>
2022-11-16 00:26:32 +0100

    Added initial implementations for CacheService, Mapper, and ParkingSpotProvider.

---
+++ ParkingService/Infrastructure/CacheService.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using ParkingService.Application;
+using ParkingService.Cache;
+using StackExchange.Redis;
+
+namespace ParkingService.Infrastructure;
+
+public class CacheService : ICacheService
+{
+    private readonly IDatabase _cache;
+
+    public CacheService()
+    {
+        this._cache = ConnectionHelper.Connection.GetDatabase();
+    }
+
+    public T? GetData<T>(string key)
+    {
+        RedisValue value = this._cache.StringGet(key);
+        return !string.IsNullOrEmpty(value) ? JsonConvert.DeserializeObject<T>(value!) : default;
+    }
+
+    public void SetData<T>(string key, T value, DateTimeOffset expirationTime)
+    {
+        TimeSpan expiryTime = expirationTime.DateTime.Subtract(DateTime.Now);
+        this._cache.StringSet(key, JsonConvert.SerializeObject(value), expiryTime);
+    }
+}
---
+++ ParkingService/Infrastructure/ParkingSpotProvider.cs
@@ -0,0 +1,24 @@
+﻿using ParkingService.Application;
+using ParkingService.Domain;
+using ParkingService.ParkingSpotApi;
+using ParkingService.ParkingSpotApi.Responses;
+
+namespace ParkingService.Infrastructure;
+
+public class ParkingSpotProvider : IParkingSpotProvider
+{
+    private readonly IParkingSpotApi _parkingSpotApi;
+    private readonly IMapper _mapper;
+    
+    public ParkingSpotProvider(IParkingSpotApi parkingSpotApi, IMapper mapper)
+    {
+        this._parkingSpotApi = parkingSpotApi;
+        this._mapper = mapper;
+    }
+
+    public async Task<IEnumerable<ParkingSpot>> GetParkingSpotsAsync()
+    {
+        ParkingSpotApiResponse parkingSpots = await this._parkingSpotApi.GetParkingSpotsAsync();
+        return parkingSpots.ParkingSpots.Select(this._mapper.MapParkingSpotResponseToParkingSpot);
+    }
+}
